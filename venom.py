960657eba9cf4b66b5f9a6a6d601ae543c1eacdc
uksd7 <ronald60mitche.ll.4.058@gmail.com>
2024-08-04 21:12:03 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7255048090:AAH2_TjHO-6Kk8WVZiwPdbZMfSPUJhwrZNY'
-ADMIN_ID = '5588464519'
+API_TOKEN = '7299114738:AAHf_rPMVq0xVkN284_3k-uMrSCmvh_Hsfs'
+ADMIN_ID = '1761118584'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
